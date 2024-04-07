b7c26b429cb7369917701f73b3d194cfa5a0658c
Sarim Rehan <166250973+sarimrehan12@users.noreply.github.com>
2024-04-07 21:32:34 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,123 @@
+//let fullName :string="muhammad sarim";
+//console.log(fullName)
+//console.log(fullName)
+//console.log(fullName)
+/*
+console.log(fullName)
+jsanfd
+sdnjas
+asdfjireew
+*/
+/*
+let num1: number= 2;
+let num2: number= 3;
+let total: number= num1 + num2;
+console.log(total)
+//subtraction operator
+let num3: number= 5;
+let num4: number= 2;
+let cart: number= num3 - num4;
+console.log(cart)
+*/
+/*
+let totalApples: number=20
+let bags: number=4
+let eachBags: number=totalApples / bags
+console.log(eachBags)
+*/
+/*
+addition operators
+let num1: number=2;
+let num2: number=3;
+let totalApples:number= num1 + num2;
+console.log(totalApples)
+*/
+/*subtraction operator
+let num1: number=5;
+let num2: number=2;
+let totalApples:number= num1 - num2;
+console.log(totalApples)*/
+/* multiply answers
+let bags : number =4;
+let apples : number =5;
+let totalApples:number= bags * apples
+console.log(totalApples)*/
+/*division operators
+let totalApples: number =20;
+let bags: number =4;
+let eachBags: number =totalApples / bags;
+console.log(eachBags)
+*/
+/*exponention
+let baseNumber : number =5;
+let powerNumber : number =6;
+let power : number= baseNumber ** powerNumber
+console.log(power)*/
+//modules operator 
+//let num1: number= 5;
+//let num2: number= 8;
+//pre increment 
+//console.log("line:61",++num1);
+//console.log("line: 62:",num1++);
+//console.log("line: 63:",num1);
+//let num2 :number= 8;
+//pre decrement
+//console.log(--num2)
+//post decrement
+//console.log ("line:68:",num2--);
+//console.log("line:69:",num2);
+//combining operators 
+//let result = 3 + 4 * 5;
+//console.log(result)
+//comparison operators
+/*let num4 :number= 10;
+let num5 :number= 4;
+let eaqualsCompariosn =num4 == num5
+console.log(eaqualsCompariosn);
+let notEaqualsCompariosn =num4 != num5
+console.log(notEaqualsCompariosn);
+let greaterThanCompariosn =num4 > num5
+console.log(greaterThanCompariosn);
+let lessThanCompariosn =num4 < num5
+*/
+// let a=5
+// let b=6
+// let logicalAnd = (a > 0) && (a > 0);
+// let logicalOr = (a < 0) || (a > 0) || (a > 0);
+// let logicalNot = !(a < 0);
+// console.log(logicalAnd);
+// console.log(logicalOr);
+// console.log(logicalNot)
+// let isRaining =true;
+// if(isRaining) {
+//     console.log("take an umbrella" );
+// }else{
+//     console.log("wear sun glasses");
+// }
+// let isNotRaining= false;
+// if(isNotRaining){
+//     console.log("take umbrella");
+// }else{
+//     console.log("wear sunglasses");
+// }
+// let washedClothes = false;
+// if (washedClothes) {
+//     console.log("dry")
+// }else{
+//     console.log("wet")
+// }
+// let weather = "cloudy";
+// if(weather==="raining"){
+//     console.log("wear a raincoat");
+// }else if(weather==="cloudy"){
+//     console.log("wear a light jacket")
+// }else{console.log("wear sunglasses")} 
+// let weather="storm";
+// if(weather=="raining"){
+//     console.log("wear a raincoat")
+// }else if(weather=="sunny"){
+//     console.log("drink water")
+// }else{console.log("stay at home")}
+var isHungry = false;
+var snack = isHungry ? "apple" : "water";
+console.log("you should have some ".concat(snack, "."));
